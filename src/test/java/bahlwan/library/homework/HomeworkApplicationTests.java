3687d75579cadfc79343322cf3f12bd6deae44e9
AhmadBahlwan <ahmad.abdalziz.bhlwan@gmail.com>
2021-12-03 23:28:49 +0200

    starter project (empty)

---
+++ src/test/java/bahlwan/library/homework/HomeworkApplicationTests.java
@@ -0,0 +1,15 @@
+package bahlwan.library.homework;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomeworkApplicationTests {
+
+	@Test
+	void contextLoads() {
+		Assertions.assertTrue(true);
+	}
+
+}
